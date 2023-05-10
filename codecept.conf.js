3fe07f3b06e88f40f5de008523bc9daabfb7b3d5
garbuzpb <garbuz.pb@gmail.com>
2023-05-10 12:20:40 +0200

    first commit

---
+++ codecept.conf.js
@@ -0,0 +1,62 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS)
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins()
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+		REST: {},
+    Playwright: {
+      url: 'https://demoqa.com',
+      show: true,
+      browser: 'chromium',
+			chromium: {
+        args: [
+          // "--start-maximized",
+        ],
+        ignoreHTTPSErrors: true,
+      },
+			// windowSize: "1920x1000",
+			waitForNavigation: "domcontentloaded", // ["networkidle2"]
+      waitForAction: 200,
+    }
+  },
+	mocha: {
+    reporterOptions: {
+      "codeceptjs-cli-reporter": {
+        stdout: "-",
+        options: {
+          verbose: false,
+          steps: true,
+        },
+      },
+      "mocha-junit-reporter": {
+        stdout: "./output/console.log",
+        options: {
+          mochaFile: "./output/result.xml",
+        },
+      },
+    },
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'codeceptjs_2023',
+	plugins: {
+		pauseOnFail: {},
+		retryFailedStep: {
+      enabled: true,
+      retries: 3,
+      defaultIgnoredSteps: ["wait*", "execute*", "run*", "have*", "send*"],
+    },
+		screenshotOnFail: {
+      enabled: true,
+    },
+	}
+}
---
+++ firstFeature_test.js
@@ -0,0 +1,11 @@
+Feature('firstFeature');
+
+Scenario('test something',  ({ I }) => {
+	I.amOnPage('/')
+	I.see('Forms')
+	I.click({xpath: '//h5[text()=\'Elements\']'})
+	I.click({xpath: '//span[text()=\'Web Tabl00es\']'})
+	console.log('process.env.HEADLESS: ', process.env.HEADLESS)
+	I.click('#delete-record-1')
+	// pause()
+});
