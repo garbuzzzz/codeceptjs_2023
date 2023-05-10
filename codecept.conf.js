84f82bb81857bf3eb5d8d2353f5792d3558677d6
garbuzpb <garbuz.pb@gmail.com>
2023-05-10 15:55:58 +0200

    eslint

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,4 +1,4 @@
-const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure')
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS)
@@ -8,55 +8,55 @@ setCommonPlugins()
 
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-  tests: './*_test.js',
-  output: './output',
-  helpers: {
+	tests: './*_test.js',
+	output: './output',
+	helpers: {
 		REST: {},
-    Playwright: {
-      url: 'https://demoqa.com',
-      show: true,
-      browser: 'chromium',
+		Playwright: {
+			url: 'https://demoqa.com',
+			show: true,
+			browser: 'chromium',
 			chromium: {
-        args: [
-          // "--start-maximized",
-        ],
-        ignoreHTTPSErrors: true,
-      },
+				args: [
+					// "--start-maximized",
+				],
+				ignoreHTTPSErrors: true,
+			},
 			// windowSize: "1920x1000",
-			waitForNavigation: "domcontentloaded", // ["networkidle2"]
-      waitForAction: 200,
-    }
-  },
+			waitForNavigation: 'domcontentloaded', // ["networkidle2"]
+			waitForAction: 200,
+		}
+	},
 	mocha: {
-    reporterOptions: {
-      "codeceptjs-cli-reporter": {
-        stdout: "-",
-        options: {
-          verbose: false,
-          steps: true,
-        },
-      },
-      "mocha-junit-reporter": {
-        stdout: "./output/console.log",
-        options: {
-          mochaFile: "./output/result.xml",
-        },
-      },
-    },
-  },
-  include: {
-    I: './steps_file.js'
-  },
-  name: 'codeceptjs_2023',
+		reporterOptions: {
+			'codeceptjs-cli-reporter': {
+				stdout: '-',
+				options: {
+					verbose: false,
+					steps: true,
+				},
+			},
+			'mocha-junit-reporter': {
+				stdout: './output/console.log',
+				options: {
+					mochaFile: './output/result.xml',
+				},
+			},
+		},
+	},
+	include: {
+		I: './steps_file.js'
+	},
+	name: 'codeceptjs_2023',
 	plugins: {
 		pauseOnFail: {},
 		retryFailedStep: {
-      enabled: true,
-      retries: 3,
-      defaultIgnoredSteps: ["wait*", "execute*", "run*", "have*", "send*"],
-    },
+			enabled: true,
+			retries: 3,
+			defaultIgnoredSteps: ['wait*', 'execute*', 'run*', 'have*', 'send*'],
+		},
 		screenshotOnFail: {
-      enabled: true,
-    },
+			enabled: true,
+		},
 	}
 }
--- firstFeature_test.js
+++ firstFeature_test.js
@@ -1,11 +1,11 @@
-Feature('firstFeature');
+Feature('firstFeature')
 
 Scenario('test something',  ({ I }) => {
 	I.amOnPage('/')
 	I.see('Forms')
-	I.click({xpath: '//h5[text()=\'Elements\']'})
-	I.click({xpath: '//span[text()=\'Web Tabl00es\']'})
+	I.click({ xpath: '//h5[text()=\'Elements\']' })
+	I.click({ xpath: '//span[text()=\'Web Tables\']' })
 	console.log('process.env.HEADLESS: ', process.env.HEADLESS)
 	I.click('#delete-record-1')
 	// pause()
-});
+})
